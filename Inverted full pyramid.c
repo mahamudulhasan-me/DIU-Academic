39d1c1b96b142f2614f8d69117136cf0cfa40500
Mahamudul Hasan <95156580+MahamudulHasanx@users.noreply.github.com>
2022-03-13 10:46:43 +0600

    Update Inverted full pyramid.c

--- Inverted full pyramid.c
+++ Inverted full pyramid.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main() {
-   int rows, i, j, space;
+   int i, space, rows, k = 0;
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
-   for (i = rows; i >= 1; --i) {
-      for (space = 0; space < rows - i; ++space)
+   for (i = 1; i <= rows; ++i, k = 0) {
+      for (space = 1; space <= rows - i; ++space) {
          printf("  ");
-      for (j = i; j <= 2 * i - 1; ++j)
+      }
+      while (k != 2 * i - 1) {
          printf("* ");
-      for (j = 0; j < i - 1; ++j)
-         printf("* ");
+         ++k;
+      }
       printf("\n");
    }
    return 0;
 }
-
